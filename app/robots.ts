5b75b3f6d0464a350b7f59ec5c03f9ffe6cb3ebe
megamo1362 <meghdadi.ma@gmail.com>
2026-07-13 16:23:39 +0330

    Refactor robots function to use array for rules

--- app/robots.ts
+++ app/robots.ts
@@ -2,11 +2,25 @@ import type { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: ['/dashboard', '/admin', '/api'],
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/dashboard', '/admin', '/api'],
+      },
+      {
+        userAgent: 'Google-Extended',
+        allow: '/',
+      },
+      {
+        userAgent: 'GPTBot',
+        allow: '/',
+      },
+      {
+        userAgent: 'ClaudeBot',
+        allow: '/',
+      },
+    ],
     sitemap: 'https://mindlura.com/sitemap.xml',
   };
 }
